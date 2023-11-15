9338d02da7e9fa50d8eefa8b05a85c6440442411
Mirza <81905418+mirza-hrustanovic@users.noreply.github.com>
2023-11-16 00:28:19 +0100

    added Center widget to center images and added textbutton with void function onPressed

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -1,32 +1,45 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text("Dice Game"),
         backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text("Dice Game"),
-          backgroundColor: Colors.red,
-          centerTitle: true,
-        ),
-        body: Dice(),
+        centerTitle: true,
       ),
-    )
-  );
+      body: Dice(),
+    ),
+  ));
 }
-class Dice extends StatelessWidget{
+
+class Dice extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Row(
-      children:<Widget>[
-        Expanded(child:
-       Image.asset("images/dice1.png")
-        ),
-        Expanded(child:
-        Image.asset("images/dice1.png"),
-        ),
-      ],
+  Widget build(BuildContext context) {
+    var leftDice = 5;
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child:TextButton(
+                onPressed: (){
+                  print("Left button is pressed");
+                },
+                child: Image.asset("images/dice$leftDice.png"),
+              ),
+
+          ),
+          Expanded(
+            child:TextButton(
+              onPressed:(){
+                print("Right button is pressed");
+              },
+              child:  Image.asset("images/dice1.png"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
